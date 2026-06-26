d371ed05c8c70eda83228d0aab8a180afb3e9309
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2026-06-26 12:14:41 -0500

    Fixes multi_predict for resnet

--- R/tabular_resnet.R
+++ R/tabular_resnet.R
@@ -184,7 +184,7 @@ multi_predict._brulee_resnet <-
     load_libs(object, quiet = TRUE, attach = TRUE)
 
     if (is.null(epochs)) {
-      epochs <- length(object$fit$estimates)
+      epochs <- length(object$fit$estimates) - 1L
     }
 
     epochs <- sort(epochs)
